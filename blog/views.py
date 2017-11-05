c958d024fe9770bdf13efda2a418f3853156f2bd
pyyzzang <zzangpyy@naver.com>
2017-11-05 21:37:31 +0900

    delete

--- blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-	
-def google(request):
-    return render(request, 'blog/google.html', {})
