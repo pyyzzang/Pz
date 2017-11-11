77fa1ad57f9d8fae10499a0cd61507cdcf6662a7
pyyzzang <zzangpyy@naver.com>
2017-11-11 12:46:14 +0900

    aaa

--- HomePage/app/subViews/fileListView.py
+++ HomePage/app/subViews/fileListView.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 import os
+import pathlib
 
 class fileListView(object):
     def getFileList(ext):
         http = "<http>"
-        for (path, dir, files) in os.walk("C:\Temp"):
+        http += "<table border='1'> " 
+        
+        for (path, dir, files) in os.walk("/home/pi/Downloads"):
             for file in files:
-                http = http + file
+                http += "<tr>"
+                http = http + "<td>" + file + "</td>"
+                http = http + "<td><button width=\"100\" height=\"100\"/></td>"
+                http += "</tr>"
+        http += "</table>"
         http = http + "</http>"
         return HttpResponse(http)
