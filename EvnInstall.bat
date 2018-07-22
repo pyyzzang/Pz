67d282ae40e394b13f14341913fec99adede6998
yypark <zzangpyy@naver.com>
2018-07-22 17:39:26 +0900

    Python pip업데이트 추가.

--- EvnInstall.bat
+++ EvnInstall.bat
@@ -1,6 +1,8 @@
 set Python_Exe="C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\Python.exe"
 set PIP_Exe="C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\Scripts\pip.exe"
 
+%Python_Exe% -m pip install --upgrade pip
+
 %PIP_Exe% install -e django
 
 %PIP_Exe% install --upgrade django
