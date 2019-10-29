25cfe61c67cf40ac7c30fb49165b661aa59735b1
yypark <pyyzzang@gmail.com>
2019-10-29 01:41:44 +0000

    add button image

--- HomePage/app/subViews/playView.py
+++ HomePage/app/subViews/playView.py
@@ -17,14 +17,32 @@ class playView(object):
 
         #moviPlayer = str(win32gui.FindWindow("ApplicationFrameWindow", None))
         http += "<html> <script src=\"http://code.jquery.com/jquery-1.11.2.min.js\"></script>"
-
+        http += "<Head> <link rel='stylesheet' href='/static/app/css/style.css'> </Head>";
         if(0):
             http += "Not Play Media.<p>"
         else:
             http += "<script type=\"text/javascript\">"
-            #Back
+            #Back600
             http += "$(function(){"
-            http += "$(\"#BackButton\").click(function(){"
+            http += "$(\"#Back600Button\").click(function(){"
+            http += "$.ajax({"
+            http += "type: 'get'"
+            http += ", url: 'Back600'"
+            http += ", dataType : 'html'"
+            http += ", error : function(){"
+            http += "alert('fail!!');"
+            http += "}"
+            http += ", success : function(data){"
+            http += "}"
+            http += "});"
+            http += "})"
+            http += "})"
+            http += "</script>"
+
+
+            #Back10
+            http += "$(function(){"
+            http += "$(\"#Back600Button\").click(function(){"
             http += "$.ajax({"
             http += "type: 'get'"
             http += ", url: 'Back'"
@@ -39,10 +57,61 @@ class playView(object):
             http += "})"
             http += "</script>"
 
-            #Skip
+            #Play
+            http += "$(function(){"
+            http += "$(\"#Back600Button\").click(function(){"
+            http += "$.ajax({"
+            http += "type: 'get'"
+            http += ", url: 'Play'"
+            http += ", dataType : 'html'"
+            http += ", error : function(){"
+            http += "alert('fail!!');"
+            http += "}"
+            http += ", success : function(data){"
+            http += "}"
+            http += "});"
+            http += "})"
+            http += "})"
+            http += "</script>"
+
+            #Pause
+            http += "$(function(){"
+            http += "$(\"#Back600Button\").click(function(){"
+            http += "$.ajax({"
+            http += "type: 'get'"
+            http += ", url: 'Pause'"
+            http += ", dataType : 'html'"
+            http += ", error : function(){"
+            http += "alert('fail!!');"
+            http += "}"
+            http += ", success : function(data){"
+            http += "}"
+            http += "});"
+            http += "})"
+            http += "})"
+            http += "</script>"
+
+            #Skip10
+            http += "$(function(){"
+            http += "$(\"#Back600Button\").click(function(){"
+            http += "$.ajax({"
+            http += "type: 'get'"
+            http += ", url : 'Skip'"
+            http += ", dataType : 'html'"
+            http += ", error : function(){"
+            http += "alert('fail!!');"
+            http += "}"
+            http += ", success : function(data){"
+            http += "}"
+            http += "});"
+            http += "})"
+            http += "})"
+            http += "</script>"
+
+            #Skip600
             http += "<script type=\"text/javascript\">"
             http += "$(function(){"
-            http += "$(\"#SkipButton\").click(function(){"
+            http += "$(\"#Skip600Button\").click(function(){"
             http += "$.ajax({"
             http += "type: 'get'"
             http += ", url: 'Skip'"
@@ -93,10 +162,26 @@ class playView(object):
             http += "})"
             http += "</script>"
 
-            http += "<input type='button' id=\"BackButton\" value='Back' />"
-            http += "<input type='button' id=\"SkipButton\" value='Skip'/>"
-            http += "<input type='button' id=\"VolumeUpButton\" value='VolumeUp'/>"
-            http += "<input type='button' id=\"VolumeDownButton\" value='VolumeDown'/>"
+            http += "<body>"
+
+            http += "<div class='main'>                                                                             ";
+            http += "	<div class='buttonDiv'><button id='Back600Button' class='back600'/>	</div>                  ";
+            http += "	<div class='buttonDiv'><button id='Back10Button' class='back10'/>	</div>                      ";
+            http += "	<div class='buttonDiv'><button id='PlayerButton' class='play' /> </div>                     ";
+            http += "	<div class='buttonDiv'><button id='PauseButton' class='pause'/>	</div>                  ";
+            http += "	<div class='buttonDiv'><button id='Skip10Button' class='skip10'/>	</div>                      ";
+            http += "	<div class='buttonDiv'><button id='Skip600Button' class='skip600'/>	</div>                  ";
+            http += "</div>                                                                                         ";
+            http += "<div class='Empty20'>                                                                          ";
+            http += "	<label> </label>                                                                            ";
+            http += "</div>                                                                                         ";
+            http += "<div class='volumeMain'>                                                                       ";
+            http += "	<div class='volumeDiv'><button id='VolumeUpButton' class='volumeUp'/>	</div>              ";
+            http += "	<div class='volumeDiv'><button id='VolumeDownButton' class='volumeDown'/>	</div>	        ";
+            http += "</div>                                                                                         ";
+            http += "</body>                                                                                        ";
+
+
             http += "</html>"
 
         return HttpResponse(http)
