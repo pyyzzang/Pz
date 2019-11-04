be22f6fd3994733e3a2fd403532f118a7cd91d77
PyyZzAng <zzangpyy@naver.com>
2019-11-04 16:00:05 +0900

    정규식 기능 추가.

--- HomePage/app/subViews/testView.py
+++ HomePage/app/subViews/testView.py
@@ -4,10 +4,10 @@ import os
 
 os.environ['DISPLAY'] = ':0'
 
-from omxplayer.player import OMXPlayer
+#from omxplayer.player import OMXPlayer
 
-import pynput
-import keyboard
+#import pynput
+#import keyboard
 from django.http import HttpResponse
 import os.path
 import os,sys
@@ -33,12 +33,34 @@ class testView():
          localFilePath = osDefine.LocalFilePath()
          http = "";
          fileCount = 0;
-         for (path, dir, files) in os.walk(localFilePath):
-            for file in files:
-                if(-1 != path.find(localFilePath)):
-                     file = path.replace(localFilePath,'') + '/' + file;
-                    
-                http += "file : " + str(strUtil.getMatchTitle(file)) + " isTitle : " + file  ;
+         files = [  "[tvN] 수요일은 음악프로.E01.191002.450p-MreD.mp4	"
+                    ,"그것이 알고 싶다.E1189.191102.720p-NEXT.mp4	"
+                    ,"나쁜 녀석들 더 무비 THE BAD GUYS REIGN OF CHAOS.2019.1080p.FHDRip.H264.AAC.mp4	"
+                    ,"백종원의 골목식당.E90.191030.720p-NEXT.mp4	"
+                    ,"수요일은 음악프로.E04.191023.720p-NEXT.mp4	"
+                    ,"수요일은 음악프로.E05.191030.720p-NEXT.mp4	"
+                    ,"시베리아 선발대.E01.190926.720p-NEXT.mp4	"
+                    ,"시베리아 선발대.E02.191003.720p-NEXT.mp4	"
+                    ,"시베리아 선발대.E03.191010.720p-NEXT.mp4	"
+                    ,"시베리아 선발대.E04.191017.720p-NEXT.mp4	"
+                    ,"시베리아 선발대.E05.191024.720p-NEXT.mp4	"
+                    ,"시베리아 선발대.E06.191031.720p-NEXT.mp4	"
+                    ,"쌉니다 천리마마트.E07.191101.720p-NEXT.mp4	"
+                    ,"책 읽어드립니다.E06.191029.720p-NEXT.mp4	"
+                    ,"수요일은 음악프로.E02.191009.720p-NEXT/수요일은 음악프로.E02.191009.720p-NEXT.mp4	"
+                    ,"수요일은 음악프로.E03.191016.720p-NEXT/수요일은 음악프로.E03.191016.720p-NEXT.mp4	"
+                    ,"[tvN] 도깨비.E01-E16.720p-NEXT/[tvN] 도깨비 스페셜.170114.모든 날이 좋았다.720p-NEXT.mp4	"
+                    ,"[tvN] 도깨비.E01-E16.720p-NEXT/[tvN] 도깨비.E10.161231.720p-NEXT.mp4	"
+                    ,"[tvN] 도깨비.E01-E16.720p-NEXT/[tvN] 도깨비.E11.170106.720p-NEXT.mp4	"
+                    ,"[tvN] 도깨비.E01-E16.720p-NEXT/[tvN] 도깨비.E12.170107.720p-NEXT.mp4	"
+                    ,"[tvN] 도깨비.E01-E16.720p-NEXT/[tvN] 도깨비.E13.170113.720p-NEXT.mp4	"
+                    ,"[tvN] 도깨비.E01-E16.720p-NEXT/[tvN] 도깨비.E14.170120.720p-NEXT.mp4	"
+                    ,"[tvN] 도깨비.E01-E16.720p-NEXT/[tvN] 도깨비.E15.170121.720p-NEXT.mp4	"
+                    ,"[tvN] 도깨비.E01-E16.720p-NEXT/[tvN] 도깨비.E16.END.170121.720p-NEXT.mp4	"
+                    ,"청일전자 미쓰리.E12.191031.720p-NEXT/청일전자 미쓰리.E12.191031.720p-NEXT.mp4"];
+            
+         for file in files:
+                http += "file : " + str(strUtil.getMatchTitle(file));
                 http += "<p>";
          return HttpResponse(http); 
 
