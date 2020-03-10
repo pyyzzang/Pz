e90cfaa51a8ec11aa759b780707b40f95ea5d392
pyyzzang <pyyzzang@gmail.com>
2020-03-10 15:43:17 +0900

    폴더 접근 권한 777로 변경 함.

--- Test.py
+++ Test.py
@@ -1,6 +1,5 @@
 #-*- coding:utf-8 -*-
-
-
+'''
 import sys
 
 import pyodbc
@@ -14,61 +13,21 @@ connectString = "DRIVER="+driver+";SERVER="+server+";PORT=1433;DATABASE="+databa
 print(connectString);
 cnxn = pyodbc.connect(connectString);
 cursor = cnxn.cursor();
-cursor.execute("select * from Title");
-rows = cursor.fetchall();
-for row in rows:
-    t = row[0];
-    t = t.encode('ISO-8859-1')
-    t = t.decode('euc-kr')
-    print(t);
+cursor.execute("insert into Torrent values('1111', ' ', '11', GETDATE(), 7)");
+cnxn.commit();
+cnxn.close();
 '''
-
-str = u'\xc7\xd1\xb1\xdb';
-
-
-strb = str.encode("euc-kr");
-ustr = b'\xa4\xa1\xa4\xa1';
-print(ustr.decode("euc-kr"));
-
-
-class FileInfo:
-    def __init__(self, filePath, dir):
-        self.filePath = filePath;
-        self.dir = dir;
-        self.fileName, self.ext = os.path.splitext(filePath);
-    def __lt__(self, other):
-        return self.getTitle() < other.getTitle(); 
-    def getFileName(self):
-        return self.fileName;
-    def getFullName(self):
-        return self.dir + "/" + self.filePath;
-    def getExt(self):
-        return self.ext;
-
-    def getTitle(self):
-        title = strUtil.getMatchTitle(self.filePath);
-        if(" " == title):
-            title = self.filePath;
-        return title;
-    def isVideoFile(self):
-        return self.getExt() in osDefine.SupportExt;
-    def isDirectory(self):
-        return os.path.isdir(self.getFullName());
-    def getEncodingFileName(self):
-        return osDefine.Base64Encoding(self.getUrlPath());
-    def getUrlPath(self):
-        if(-1 != self.dir.find(osDefine.LocalFilePath())):
-            return self.dir.replace(osDefine.LocalFilePath(), '') + '/' + self.filePath;
-        return self.filePath;
-
 import os;
-import sys;
+import subprocess;
+import base64;
+torrentUrl = "magnet-link http://192.168.219.102:8000/static/Tmp/LastUpload.Torrent";
+balue = subprocess.check_output(torrentUrl, shell = True);
+print(type(balue));
+Binary = balue.replace("\n","");
+Title = "";
+Binary = base64.b64encode(Binary);
+print(type(Binary));
 
-base = "/home/pi/Downloads";
-for fileName in os.listdir(base):
-    info = FileInfo(fileName, base);
-    print("Thumbnail" + (True and "Dir" or "File"));
-    if(True == info.isDirectory()):
-        print(info.filePath);
+query = "insert into Torrent values('%s', '%s', GETDATE())" % (Title, Binary);
 
-'''
+print(query);
