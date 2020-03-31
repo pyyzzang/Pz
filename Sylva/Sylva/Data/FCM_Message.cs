3b4abcb7a71a06ee46b48a2e2c97834c7851589b
pyyzzang <zzangpyy@naver.com>
2020-03-31 13:13:40 +0900

    리스트 아이템 추가.

---
+++ Sylva/Sylva/Data/FCM_Message.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Sylva.Data
+{
+    public class FCM_Message
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public FCM_Message(string __title, string __body)
+        {
+            Title = __title;
+            Body = __body;
+        }
+    }
+}
---
+++ Sylva/Sylva/Data/MessageListAdapter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Sylva.Data
+{
+    public class MessageListAdapter: ArrayAdapter
+    {
+        private ObservableCollection<FCM_Message> _FCM_List = null;
+        public ObservableCollection<FCM_Message> FCM_List
+        {
+            get
+            {
+                if (null == _FCM_List)
+                    _FCM_List = new ObservableCollection<FCM_Message>();
+                return _FCM_List;
+            }
+            set { }
+        }
+
+        public MessageListAdapter(MainActivity __mainActivity): base(__mainActivity, Android.Resource.Layout.SimpleListItem1)
+        {
+            mainActivity = __mainActivity;
+            FCM_List.Add(new FCM_Message("Title_ 1", "Body _ 1"));
+            FCM_List.Add(new FCM_Message("Title_ 2", "Body _ 2"));
+            FCM_List.Add(new FCM_Message("Title_ 3", "Body _ 3"));
+            FCM_List.Add(new FCM_Message("Title_ 4", "Body _ 4"));
+            this.AddAll(FCM_List);
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View v = convertView;
+            FCM_Message msg = FCM_List[position];
+            if (v == null) // no view to re-use, create new
+                v = mainActivity.LayoutInflater.Inflate(Resource.Layout.MessageLayout, null);
+            if (null != msg)
+            {
+                TextView tt = (TextView)v.FindViewById(Resource.Id.txtTitle);
+                TextView bt = (TextView)v.FindViewById(Resource.Id.txtBody);
+                if (tt != null)
+                {
+                    tt.Text = "Title : " + msg.Title;
+                }
+                if (bt != null)
+                {
+                    bt.Text = "Body : " + msg.Body;
+                }
+            }
+            return v;
+        }
+
+        private MainActivity mainActivity = null;
+
+        private List<string> list;
+        
+    }
+}
