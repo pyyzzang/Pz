59408c3b60cd56d9b3ab298d981adab19d6b9195
pyyzzang <pyyzzang@gmail.com>
2020-04-03 19:11:21 +0900

    폴더 이동 코드 추가.

--- HomePage/HomePage/TransmissionSH/AutoDelete.sh
+++ HomePage/HomePage/TransmissionSH/AutoDelete.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SERVER="9091 --auth pi:cndwn5069()"
 TORRENTLIST=`transmission-remote $SERVER --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited --delimiter=" " --fields=1`
+LOG_FILE="Bach.log"
 for TORRENTID in $TORRENTLIST
 do
     DL_COMPLETED=`transmission-remote $SERVER --torrent $TORRENTID --info | grep "Percent Done: 100%"`
@@ -9,6 +10,8 @@ do
 
         CompleteMagnet=`transmission-remote $SERVER --torrent $TORRENTID --info | grep -e Magnet:`
         if [ "$CompleteMagnet" ]; then
+            cd "/home/pi/Pz/HomePage/HomePage/TransmissionSH"
+            SHELL_PATH=`pwd -P`
             python3 "UpdateThumbnail.py" $CompleteMagnet
         fi
         
