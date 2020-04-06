ec0b6361c00a857614556483d372b6cd6cd4deda
pyyzzang <pyyzzang@gmail.com>
2020-04-06 12:41:08 +0900

    중복 추가시 오류 수정.

--- HomePage/app/subViews/torrent.py
+++ HomePage/app/subViews/torrent.py
@@ -137,9 +137,10 @@ class torrent:
         query = "select * from Torrent where magnetUrl='%s'" % (base64Magnet);
         osDefine.Logger("selectQuery : " + query);
         rows = session.QueryExecute(query);
-        if None != rows.fetchone():
+        row = rows.fetchone();
+        if None != row:
             osDefine.Logger("rows.cursor.arraysize : " + str(rows.cursor.arraysize));
-            osDefine.Logger("Equals Torrent Exists " + rows.fetchone()[0]);
+            osDefine.Logger("Equals Torrent Exists " + row[0]);
             return HttpResponse("<script> location.href='" + osDefine.getRunIp() + "/Torrent'</script>");
 
         query = "insert into Torrent (Title, MagnetUrl, modifyDate, ThumbnailImage) values ('%s', '%s', GETDATE(), '')" % (Title, base64Magnet);
