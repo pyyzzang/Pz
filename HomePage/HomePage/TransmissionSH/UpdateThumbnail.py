19ec669ea4ecfe39b9d878bbbd74ea330a3f54fe
pyyzzang <pyyzzang@gmail.com>
2020-05-01 13:02:02 +0900

    IP변경

--- HomePage/HomePage/TransmissionSH/UpdateThumbnail.py
+++ HomePage/HomePage/TransmissionSH/UpdateThumbnail.py
@@ -14,9 +14,7 @@ def getIsDev():
     return False;
 
 def getRunIp():
-    if(True == getIsDev()):
-        return "https://192.168.219.102:8080"
-    return "https://192.168.219.102:80"
+    return "https://192.168.219.102"
 
 def Base64Encoding(utfString):
     baseByte = base64.b64encode(utfString.encode("utf-8"));
@@ -44,10 +42,11 @@ if __name__ =='__main__':
     try:
         context = ssl._create_unverified_context();
         test_url = getRunIp() + "/Torrent/TorrentDownloadComplete";
+        print(test_url);
         data = urllib.parse.urlencode({"name" : name, "MagnetUrl" : baseUsMagnetUrl});
         req = urllib.request.Request(test_url, data=data.encode("utf-8"))
         response = urllib.request.urlopen(req, context=context);
-        Write("Response : " + response);
+        #Write(response);
         result =  response.read().decode("utf-8")
     except Exception as e:
         print(e);
