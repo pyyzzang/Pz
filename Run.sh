e72bd02ae12f1665347c64d0b528e70ed159cbca
pyyzzang <pyyzzang@gmail.com>
2020-05-28 10:29:24 +0900

    서버 실행 주석 제거.

--- Run.sh
+++ Run.sh
@@ -1,4 +1,4 @@
 cd /home/pi/Sylva/Pz/HomePage
 mkdir log
 sudo chmod 777 log
-#sudo python3 manage.py runsslserver 192.168.219.102:443 --certificate django.cert --key django.key
+sudo python3 manage.py runsslserver 192.168.219.102:443 --certificate django.cert --key django.key
