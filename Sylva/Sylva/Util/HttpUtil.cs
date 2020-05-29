8801bea33f7e7ab21c2d251eca7efa99a908f3b7
pyyzzang <zzangpyy@naver.com>
2020-05-29 14:50:34 +0900

    로그 파일 업로드 코드 추가.

--- Sylva/Sylva/Util/HttpUtil.cs
+++ Sylva/Sylva/Util/HttpUtil.cs
@@ -21,7 +21,11 @@ namespace Sylva.Util
         {
             get
             {
+#if DEBUG
+                return "https://192.168.219.102:8080";
+#else
                 return "https://192.168.219.102";
+#endif
             }
         }
 
@@ -43,11 +47,7 @@ namespace Sylva.Util
             ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
 
             string uploadUrl = string.Format(__url, ServerUrl);
-            using (WebClient client = new WebClient())
-            {
-                client.UploadFile(uploadUrl, __filePath);
-            }
-
+            
             string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
             byte[] boundaryBytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uploadUrl);
@@ -60,7 +60,7 @@ namespace Sylva.Util
                 requestStream.Write(boundaryBytes, 0, boundaryBytes.Length);
 
                 //string header = "Content-Disposition: form-data; name=\"" + pair.Key + "\"; filename=\"" + file.Name + "\"\r\nContent-Type: " + file.ContentType + "\r\n\r\n";
-                string header = "Content-Disposition: form-data; name=\"torrent_files\"; + filename=\"" + Path.GetFileName(__filePath) + "\"\r\nContent-Type:application/octet-stream\r\n\r\n";
+                string header = "Content-Disposition: form-data; name=\"logFile\"; filename=\"" + Path.GetFileName(__filePath ) + "\";\r\nContent-Type:multipart/form-data;\r\n\r\n";
                 byte[] bytes = System.Text.Encoding.UTF8.GetBytes(header);
                 requestStream.Write(bytes, 0, bytes.Length);
                 byte[] buffer = new byte[32768];
